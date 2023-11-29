e174224777555d426bca90aa64c05cbdeb2d7120
Albert Colubi <albertmc89@gmail.com>
2023-11-29 22:54:16 +0100

    Add responsive desktop to card component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,15 +1,28 @@
+import { motion } from "framer-motion";
 import "./Contact.css";
 
 const Contact = () => {
   return (
     <section className="contact" id="contact">
       <h2 className="section__title">Contact Me</h2>
-      <div className="contact-info-upper-container">
+      <motion.div
+        className="contact-info-upper-container"
+        animate={{
+          scale: [1, 1.1, 1.1, 1, 1],
+          borderRadius: ["0px", "40px", "40px", "0px", "0px"],
+        }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 2,
+        }}
+      >
         <div className="contact-info-container">
           <img src="/img/email.png" alt="Email icon" width={30} height={30} />
           <a href="mailto:albertmc89@gmail.com">albertmc89@gmail.com</a>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
