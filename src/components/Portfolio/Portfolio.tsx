03a418dff60d2c5ddf61ec14aa5407cd0202e749
Albert Colubi <albertmc89@gmail.com>
2024-01-31 18:20:35 +0100

    Update

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -13,8 +13,26 @@ const Portfolio = (): React.ReactElement => {
               img2={"/img/proyecto1B.webp"}
               img3={"/img/proyecto1C.webp"}
               img4={"/img/proyecto1D.webp"}
+              img5={"/img/proyecto1E.webp"}
+              img6={"/img/proyecto1F.webp"}
+              img7={"/img/proyecto1G.webp"}
+              img8={"/img/proyecto1H.webp"}
               title={"Fantasy Legends"}
               url="https://fantasy-legends.netlify.app"
+            />
+          </li>
+          <li className="projects__portfolio">
+            <Card
+              img={"/img/ProjectNetflix1.webp"}
+              img2={"/img/ProjectNetflix2.webp"}
+              img3={"/img/ProjectNetflix3.webp"}
+              img4={"/img/ProjectNetflix4.webp"}
+              img5={"/img/ProjectNetflix5.webp"}
+              img6={"/img/ProjectNetflix6.webp"}
+              img7={"/img/ProjectNetflix7.webp"}
+              img8={"/img/ProjectNetflix7.webp"}
+              title={"Netflix clone WIP"}
+              url="https://colnetflix-clone.netlify.app"
             />
           </li>
         </ul>
